098f8e12b6442956f8fc18ac9e25cf51c1ed25c4
Wolfy Meier <wolfy@wlfy.it>
2017-10-31 00:54:29 -0700

    dates

--- widgets.py
+++ widgets.py
@@ -15,6 +15,8 @@ class Clock:
 
         self.colorDay = (255, 255, 128)
         self.colorNight = (128, 128, 255)
+        self.colorToday = (0, 255, 0)
+        self.colorDate = (255, 255, 255)
 
         self.fontTime = ImageFont.truetype("fonts/Mecha.ttf", 17)
         self.dateTime = ImageFont.truetype("fonts/PixelMillennium.ttf", 8)
@@ -23,8 +25,8 @@ class Clock:
         offset = 32 * offset
         width = 32 * tiles
 
-        self.d.rectangle((offset, 0, offset, 31), fill=scaleColor((255, 8, 8), brightness))
-        self.d.rectangle((offset + width - 1, 0, offset + width - 1, 31), fill=scaleColor((8, 8, 255), brightness))
+        #self.d.rectangle((offset, 0, offset, 31), fill=scaleColor((255, 8, 8), brightness))
+        #self.d.rectangle((offset + width - 1, 0, offset + width - 1, 31), fill=scaleColor((8, 8, 255), brightness))
 
         colorTime = scaleColor(self.colorDay, brightness)
         if (time.strftime("%p") == "PM"):
@@ -34,6 +36,10 @@ class Clock:
         textWidth = self.fontTime.getsize(textTime)[0]
         self.d.text(((width - textWidth) / 2, 0), textTime, font=self.fontTime, fill=colorTime)
 
-        textDate = time.strftime("%a %d")
+        textToday = time.strftime("%A")
+        textWidth = self.dateTime.getsize(textToday)[0]
+        self.d.text(((width - textWidth) / 2, 16), textToday, font=self.dateTime, fill=scaleColor(self.colorToday, brightness))
+
+        textDate = time.strftime("%b %d")
         textWidth = self.dateTime.getsize(textDate)[0]
-        self.d.text(((width - textWidth) / 2, 18), textDate, font=self.dateTime, fill=colorTime)
+        self.d.text(((width - textWidth) / 2, 24), textDate, font=self.dateTime, fill=scaleColor(self.colorDate, brightness))
