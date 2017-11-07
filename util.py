9097c2b531bf5376b738061f6be8fccb43c4a768
Wolfy Meier <wolfy@wlfy.it>
2017-11-06 22:50:59 -0800

    auto bright with fade

--- util.py
+++ util.py
@@ -5,26 +5,28 @@ import logging
 import requests
 
 class AutoBrightness:
-    def __init__(self, briDay=200, briNight=100, briSleep=32, sunPhaseApi="http://svc.pup.haus:4287/weather/sun_phase/v1"):
+    def __init__(self, briDay=200, briNight=100, briSleep=20, sunPhaseApi="http://svc.pup.haus:4287/weather/sun_phase/v1"):
         self.briDay = briDay
         self.briNight = briNight
         self.briSleep = briSleep
 
         self.sunPhaseApi = sunPhaseApi
 
+        self.fadeTime = 30 # 30 Minutes
+
         self.timeSleepH = 23
         self.timeSleepM = 59
 
+        self.brightness = 0
+
+        # Registers
         self.dataM = -1 # Month of Sun data
         self.dataD = -1 # Data of Sun data
         self.sunriseH = -1
         self.sunriseM = -1
         self.sunsetH = -1
         self.sunsetM = -1
-
         self.lastUpdateM = -1
-
-        self.brightness = 0
 
         self.updateData()
 
@@ -54,15 +56,54 @@ class AutoBrightness:
 
         # Update values
         if now.minute != self.lastUpdateM:
-            if datetime.time(self.sunriseH, self.sunriseM) <= now.time() < datetime.time(self.sunsetH, self.sunsetM):
-                logging.debug("Updating Brightness Day")
-                self.brightness = self.briDay
-            elif datetime.time(self.sunsetH, self.sunsetM) <= now.time() < datetime.time(self.timeSleepH, self.timeSleepM):
-                logging.debug("Updating Brightness Night")
-                self.brightness = self.briNight
+
+            # Get try to update data one minute after the hour
+            if now.minute == 1 and now.day != int(self.dataD):
+                self.updateData()
+
+            sunrise = datetime.datetime(now.year,now.month,now.day,self.sunriseH,self.sunriseM,00)
+            sunset = datetime.datetime(now.year,now.month,now.day,self.sunsetH,self.sunsetM,00)
+            sleep = datetime.datetime(now.year,now.month,now.day,self.timeSleepH,self.timeSleepM,00)
+
+            fadeTime = datetime.timedelta(0,self.fadeTime * 60)
+
+            if sunrise <= now < sunset:
+                # Day
+                fadeStart = sunset - fadeTime
+                if fadeStart <= now:
+                    timeDiff = now - fadeStart
+                    minutes = timeDiff.seconds / 60
+                    logging.debug("Updating Brightness Moving to Night [{0}]".format(minutes))
+
+                    self.brightness = scaleInt(minutes, 0, self.fadeTime, self.briDay, self.briNight)
+                else:
+                    logging.debug("Updating Brightness Day")
+                    self.brightness = self.briDay
+            elif sunset <= now < sleep:
+                # Night
+                fadeStart = sleep - fadeTime
+                if fadeStart <= now:
+                    timeDiff = now - fadeStart
+                    minutes = timeDiff.seconds / 60
+                    logging.debug("Updating Brightness Moving to Sleep [{0}]".format(minutes))
+
+                    self.brightness = scaleInt(minutes, 0, self.fadeTime, self.briNight, self.briSleep)
+                else:
+                    logging.debug("Updating Brightness Night")
+                    self.brightness = self.briNight
             else:
-                logging.debug("Updating Brightness Sleep")
-                self.brightness = self.briSleep
+                # Sleep
+                fadeStart = sleep - fadeTime
+                if fadeStart <= now:
+                    timeDiff = now - fadeStart
+                    minutes = timeDiff.seconds / 60
+                    logging.debug("Updating Brightness Moving to Day [{0}]".format(minutes))
+
+                    self.brightness = scaleInt(minutes, 0, self.fadeTime, self.briSleep, self.briDay)
+                else:
+                    logging.debug("Updating Brightness Sleep")
+                    self.brightness = self.briSleep
+
             self.lastUpdateM = now.minute
 
         return self.brightness
